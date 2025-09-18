1cc3d3c2f535fc3dd66ca926ba1f4d16063069c7
Sarthak Butola <sarthakbutola@gmail.com>
2025-09-18 20:10:45 +0530

    add get all users and get user by id apis

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -150,4 +150,26 @@ router.patch("/profile", authMiddleware, async (req, res) => {
 });
 
 
+// -------------------- GET ALL USERS (ADMIN ONLY) --------------------
+router.get("/", authMiddleware, requireRole("admin"), async (req, res) => {
+  try {
+    const users = await User.find().select("-passwordHash"); // don’t expose password
+    res.json(users);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+// -------------------- GET USER BY ID (ADMIN ONLY) --------------------
+router.get("/:id", authMiddleware, requireRole("admin"), async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id).select("-passwordHash");
+    if (!user) return res.status(404).json({ message: "User not found ❌" });
+    res.json(user);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+
 export default router;
